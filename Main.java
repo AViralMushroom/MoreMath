39650e16e5a288a8907b12846e5dbe57d3c439fa
AViralMushroom <148291118+AViralMushroom@users.noreply.github.com>
2024-02-17 21:27:16 +0000

    expand test

--- Main.java
+++ Main.java
@@ -4,7 +4,7 @@ public class Main{
     public static void main(String[] args){
         long begin = System.nanoTime();
 
-        
+        System.out.println(Arrays.toString(MoreMath.expand(new double[] {2,3,4},3)));
 
         System.out.println(System.nanoTime() - begin);
     }
